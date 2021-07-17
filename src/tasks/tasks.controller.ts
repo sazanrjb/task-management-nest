0780451149eeae484f5138b278fc7018d2542f2c
Sajan Rajbhandari <sazanrjb@gmail.com>
2021-07-17 19:40:24 +0545

    Implement Authentication

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,14 +7,17 @@ import {
   Delete,
   Patch,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateStatus } from './dto/update-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './task.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])], // Inject repo any where in task module
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule], // Inject repo any where in task module
   controllers: [TasksController],
   providers: [TasksService],
 })
